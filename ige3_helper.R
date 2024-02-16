f6ca3d62b03aadd8d415f763c2dd26220d106c8c
Julien Prados <julien.prados@unige.ch>
2024-02-16 18:27:32 +0100

    simpolify the app

--- ige3_helper.R
+++ ige3_helper.R
@@ -7,12 +7,12 @@ ui <- dashboardPage(dark = NULL,help = NULL,
 		sidebarMenu(
 			sidebarHeader("Menu"),
 			menuItem(
-				"FASTQ Download from iGE3",
+				"FASTQ download",
 				tabName = "iGE3",
 				icon = icon("download")
 			),
 			menuItem(
-				"RNAseq Quantification",
+				"Read quantification",
 				tabName = "rnaseq",
 				icon = icon("calculator")
 			)
@@ -23,32 +23,22 @@ ui <- dashboardPage(dark = NULL,help = NULL,
 			tabItem(
 				tabName = "iGE3",
 				fluidRow(
-					box(title="Parameters",width=12,
+					box(title="Parameters",width=12,collapsible = FALSE,
 							textInput("ige3_link","iGE3 link",placeholder = "https://data.ige3.genomics.unige.ch/dataset/download/xxxxxxxxxx"),
-							textInput("ige3_fastq_outdir","Destination subfolder (in data/fastq/)",value = "test")
-					),
-					box(title="Run",width=12,collapsible = FALSE,
-							p(textOutput("ige3_cmd_line",container = code)),
-							actionButton("download","Download",icon = icon("download"))
-					),
-					box(title="Log",icon = icon("terminal"),width=12,
-							textOutput("ige3_terminal_stdout",container = pre)
+							textInput("ige3_fastq_outdir","Destination subfolder (in data/fastq/)",value = "test"),
+							actionButton("download","Download",icon = icon("download")),
+							verbatimTextOutput("ige3_terminal_stdout")
 					)
 				)
 			),
 			tabItem(
 				tabName = "rnaseq",
 				fluidRow(
-					box(title = "Parameters",
-							selectInput("rnaseq_genome","GENOME",choices = c("Dd+Mm","Dd","Mm","GRCh38-r45","GRCm39-M34")),
-							textInput("rnaseq_fastq_dir","FASTQ directory",value = "data/fastq/")
-					),
 					box(title="Run",width=12,collapsible = FALSE,
-							p(textOutput("rnaseq_cmd_line",container=code)),
-							actionButton("run_rnaseq","Run",icon=icon("play"))
-					),
-					box(title="Log",icon = icon("terminal"),width=12,
-							textOutput("rnseq_terminal_stdout",container = pre)
+							selectInput("rnaseq_genome","GENOME",choices = c("Dd+Mm","Dd","Mm","GRCh38-r45","GRCm39-M34")),
+							textInput("rnaseq_fastq_dir","FASTQ directory",value = "data/fastq/"),
+							actionButton("run_rnaseq","Run",icon=icon("play")),
+							verbatimTextOutput("rnseq_terminal_stdout")
 					)
 				)				
 			)
@@ -67,43 +57,30 @@ server <- function(input, output, session) {
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 	# IGE3 panel logic
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-	ige3_cmd_stdout <- reactiveVal(NULL)
-	ige3_cmd <- reactive({
-		sprintf(
+	output$ige3_terminal_stdout <- eventReactive(input$download,{
+		cmd <- sprintf(
 			"mkdir -p '%s' && wget -P '%s' --content-disposition --trust-server-names -i '%s' 2>&1",
 			file.path("data/fastq",input$ige3_fastq_outdir),
 			file.path("data/fastq",input$ige3_fastq_outdir),
 			sub("(\\.txt)?$",".txt",input$ige3_link)
 		)
-	})
-	output$ige3_cmd_line <- renderText({ige3_cmd()})
-	
-	# On Action Button Click
-	observeEvent(input$download,{ige3_cmd_stdout(pipe(ige3_cmd()))})
-	output$ige3_terminal_stdout <- renderText({
-		validate(need(!is.null(ige3_cmd_stdout()),"Not run yet"))
-		print("bbb")
-		paste(readLines(ige3_cmd_stdout()),collapse = "\n")
+		print(cmd)
+		withProgress(message="Downloading the data",{
+			pipe(cmd) |> readLines() |> paste(collapse = "\n")
+		})
 	})
 	
 	
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 	# RNASEQ panel pipeline logic
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-	rnaseq_cmd_stdout <- reactiveVal(NULL)
-	rnaseq_cmd <- reactive({sprintf("make GENOME='%s' '%s/all' 2>&1",input$rnaseq_genome,input$rnaseq_fastq_dir)})
-	output$rnaseq_cmd_line <- renderText({rnaseq_cmd()})
-	
-	# On Action Button Click
-	observeEvent(input$run_rnaseq,{rnaseq_cmd_stdout(pipe(rnaseq_cmd()))})
-	#timer <- reactiveTimer(1000)
-	output$rnseq_terminal_stdout <- renderText({
-		validate(need(!is.null(rnaseq_cmd_stdout()),"Not run yet"))
-		#timer()
-		print("aaa")
-		paste(readLines(rnaseq_cmd_stdout()),collapse = "\n")
+	output$rnseq_terminal_stdout <- eventReactive(input$run_rnaseq,{
+		cmd <- sprintf("make GENOME='%s' '%s/all' 2>&1",input$rnaseq_genome,input$rnaseq_fastq_dir)
+		print(cmd)
+		withProgress(message="Running the pipeline",{
+			pipe(cmd) |> readLines() |> paste(collapse = "\n")
+		})
 	})
-	
 }
 
 shinyApp(ui, server)
